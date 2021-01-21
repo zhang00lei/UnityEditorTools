939cc5012d1bed0073892c767414af77341319af
zhanglei <383094403@qq.com>
2021-01-21 17:08:12 +0800

    TortoiseGit提交工具

---
+++ UnityEditorTools/Assets/Editor/TortoiseGit/TortoiseEditor.cs
@@ -0,0 +1,73 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace ETEditor
+{
+    public class TortoiseEditor
+    {
+        public static string tortoiseGitPath = @"D:\Program Files\TortoiseGit\bin\TortoiseGitProc.exe";
+
+        [MenuItem("TortoiseGit/Assets/StashSave")]
+        public static void GitAssetsStushSave()
+        {
+            TortoiseGit.GitCommand(GitType.StashSave, Application.dataPath, tortoiseGitPath);
+        }
+
+        [MenuItem("TortoiseGit/Assets/StashPop")]
+        public static void GitAssetsStushPop()
+        {
+            TortoiseGit.GitCommand(GitType.StashPop, Application.dataPath, tortoiseGitPath);
+        }
+
+        [MenuItem("TortoiseGit/Assets/Push")]
+        public static void GitAssetPush()
+        {
+            TortoiseGit.GitCommand(GitType.Push, Application.dataPath, tortoiseGitPath);
+        }
+
+        [MenuItem("TortoiseGit/Assets/Log _F9")]
+        public static void GitAssetsLog()
+        {
+            string[] strs = Selection.assetGUIDs;
+            if (strs.Length > 0)
+            {
+                string path = AssetDatabase.GUIDToAssetPath(strs[0]);
+                TortoiseGit.GitCommand(GitType.Log, path, tortoiseGitPath);
+            }
+            else
+            {
+                TortoiseGit.GitCommand(GitType.Log, Application.dataPath, tortoiseGitPath);
+            }
+        }
+
+        [MenuItem("TortoiseGit/Assets/Pull _F10")]
+        public static void GitAssetsPull()
+        {
+            TortoiseGit.GitCommand(GitType.Pull, Application.dataPath, tortoiseGitPath);
+        }
+
+        [MenuItem("TortoiseGit/Assets/Commit _F11")]
+        public static void GitAssetsCommit()
+        {
+            TortoiseGit.GitCommand(GitType.Commit, Application.dataPath, tortoiseGitPath);
+        }
+
+        [MenuItem("TortoiseGit/ProjectSettings/Log")]
+        public static void GitProjectSettingsLog()
+        {
+            TortoiseGit.GitCommand(GitType.Log, Application.dataPath + "/../ProjectSettings", tortoiseGitPath);
+        }
+
+        [MenuItem("TortoiseGit/ProjectSettings/Pull")]
+        public static void GitProjectSettingsPull()
+        {
+            TortoiseGit.GitCommand(GitType.Pull, Application.dataPath + "/../ProjectSettings", tortoiseGitPath);
+        }
+
+        [MenuItem("TortoiseGit/ProjectSettings/Commit")]
+        public static void GitProjectSettingsCommit()
+        {
+            TortoiseGit.GitCommand(GitType.Commit, Application.dataPath + "/../ProjectSettings", tortoiseGitPath);
+        }
+    }
+}
---
+++ UnityEditorTools/Assets/Editor/TortoiseGit/TortoiseGit.cs
@@ -0,0 +1,123 @@
+﻿using System.Diagnostics;
+using System.IO;
+
+namespace ETEditor
+{
+    public enum GitType
+    {
+        Log,
+        Pull,
+        Commit,
+        Push,
+        StashSave,
+        StashPop,
+    }
+
+    public static class TortoiseGit
+    {
+        private const string quota = "\"";
+
+        public const string COMMAND_TORTOISE_LOG = @"/command:log /path:{0} /findtype:0 /closeonend:0";
+        public const string COMMAND_TORTOISE_PULL = @"/command:pull /path:{0} /closeonend:0";
+        public const string COMMAND_TORTOISE_COMMIT = @"/command:commit /path:{0} /closeonend:0";
+        public const string COMMAND_TORTOISE_PUSH = @"/command:push /path:{0} /closeonend:0";
+        public const string COMMAND_TORTOISE_STASHSAVE = @"/command:stashsave /path:{0} /closeonend:0";
+        public const string COMMAND_TORTOISE_STASHPOP = @"/command:stashpop /path:{0} /closeonend:0";
+
+        public static Process CreateProcess(string path, string arguments)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo(path, arguments)
+            {
+                WindowStyle = ProcessWindowStyle.Hidden,
+                UseShellExecute = false,
+                ErrorDialog = false,
+                CreateNoWindow = true,
+                RedirectStandardError = true,
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                LoadUserProfile = true
+            };
+            return new Process() { StartInfo = startInfo };
+        }
+
+        public static void GitCommand(GitType gitType, string path, string tortoiseGitPath)
+        {
+            if (!File.Exists(tortoiseGitPath))
+            {
+                UnityEngine.Debug.LogError("TortoiseGitPath can't find.");
+                return;
+            }
+
+            switch (gitType)
+            {
+                case GitType.Log:
+                    GitLog(path, tortoiseGitPath);
+                    break;
+                case GitType.Commit:
+                    GitCommmit(path, tortoiseGitPath);
+                    break;
+                case GitType.Pull:
+                    GitPull(path, tortoiseGitPath);
+                    break;
+                case GitType.Push:
+                    GitPush(path, tortoiseGitPath);
+                    break;
+                case GitType.StashSave:
+                    GitStashSave(path, tortoiseGitPath);
+                    break;
+                case GitType.StashPop:
+                    GitStashPop(path, tortoiseGitPath);
+                    break;
+                default: break;
+            }
+        }
+
+        private static void GitPush(string path, string tortoiseGitPath)
+        {
+            var args = quota + path + quota;
+            args = string.Format(COMMAND_TORTOISE_PUSH, args);
+            Process process = CreateProcess(tortoiseGitPath, args);
+            process.Start();
+        }
+
+        public static void GitStashPop(string path, string tortoiseGitPath)
+        {
+            var args = quota + path + quota;
+            args = string.Format(COMMAND_TORTOISE_STASHPOP, args);
+            Process process = CreateProcess(tortoiseGitPath, args);
+            process.Start();
+        }
+
+        public static void GitStashSave(string path, string tortoiseGitPath)
+        {
+            var args = quota + path + quota;
+            args = string.Format(COMMAND_TORTOISE_STASHSAVE, args);
+            Process process = CreateProcess(tortoiseGitPath, args);
+            process.Start();
+        }
+
+        public static void GitLog(string path, string tortoiseGitPath)
+        {
+            var args = quota + path + quota;
+            args = string.Format(COMMAND_TORTOISE_LOG, args);
+            Process process = CreateProcess(tortoiseGitPath, args);
+            process.Start();
+        }
+
+        public static void GitPull(string path, string tortoiseGitPath)
+        {
+            var args = quota + path + quota;
+            args = string.Format(COMMAND_TORTOISE_PULL, args);
+            Process process = CreateProcess(tortoiseGitPath, args);
+            process.Start();
+        }
+
+        public static void GitCommmit(string path, string tortoiseGitPath)
+        {
+            var args = quota + path + quota;
+            args = string.Format(COMMAND_TORTOISE_COMMIT, args);
+            Process process = CreateProcess(tortoiseGitPath, args);
+            process.Start();
+        }
+    }
+}
