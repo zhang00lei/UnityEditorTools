26b9722ba5834b06a0a41be4bc5e4f62a99c180c
zhanglei <383094403@qq.com>
2021-01-22 11:40:33 +0800

    切割图片非透明区域工具

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Unity编辑器工具整理
 ### TortoiseGit提交工具
-用法说明：https://www.cnblogs.com/Yellow0-0River/p/14271390.html
+用法参考：https://www.cnblogs.com/Yellow0-0River/p/14271390.html
 ### 获取Git提交日志
 用法参考：https://www.cnblogs.com/Yellow0-0River/p/14309409.html
+### 切割图片非透明区域
+用法参考：https://www.cnblogs.com/Yellow0-0River/p/14312415.html
---
+++ UnityEditorTools/Assets/Editor/SplitImgTools/SplitImgTools.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+public class SplitImgTools
+{
+    [MenuItem("Assets/SplitImg")]
+    private static void SelectSplitImg()
+    {
+        string param = "\"";
+        Texture2D[] textures = Selection.GetFiltered<Texture2D>(SelectionMode.DeepAssets);
+        foreach (Texture2D texture2D in textures)
+        {
+            string path = AssetDatabase.GetAssetPath(texture2D);
+            path = path.Replace("Assets/", string.Empty);
+            path = Path.Combine(Application.dataPath, path);
+            path = path.Replace("/", "\\");
+            param += path + ";";
+        }
+
+        param += "\"";
+        Debug.Log(param);
+        string toolsPath = Path.Combine(Application.dataPath, "../Tools/SplitImg/SplitImg.exe");
+        Process p = new Process();
+        p.StartInfo.FileName = toolsPath;
+        p.StartInfo.Arguments = param;
+        p.StartInfo.CreateNoWindow = false;
+        p.StartInfo.UseShellExecute = false;
+        p.StartInfo.RedirectStandardOutput = true;
+        p.StartInfo.WorkingDirectory = Application.dataPath;
+        p.OutputDataReceived += DataReceivedEvent;
+        p.Start();
+        p.BeginOutputReadLine();
+        p.WaitForExit();
+        p.Close();
+        p.Dispose();
+        AssetDatabase.Refresh();
+    }
+
+    private static void DataReceivedEvent(object sender, DataReceivedEventArgs e)
+    {
+        if (!string.IsNullOrEmpty(e.Data))
+        {
+            Debug.Log(e.Data);
+        }
+    }
+}
