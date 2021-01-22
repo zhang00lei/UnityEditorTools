a8d136457ad75271e1e89e4b7300c47fac45b974
zhanglei <383094403@qq.com>
2021-01-22 14:41:38 +0800

    OtherTools

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Unity编辑器工具整理
+### 其他编辑器小工具
+1. 一键启动、停止unity编辑器
+2. 清空Console日志
 ### TortoiseGit提交工具
 用法参考：https://www.cnblogs.com/Yellow0-0River/p/14271390.html
 ### 获取Git提交日志
---
+++ UnityEditorTools/Assets/Editor/OtherTools/CTools.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+using UnityEditor;
+
+public class CTools
+{
+    [MenuItem("Tools/RunOrStop _F5")]
+    private static void StartAPP()
+    {
+        EditorApplication.isPlaying = !EditorApplication.isPlaying;
+    }
+
+    [MenuItem("Tools/ClearConsole _F1")]
+    private static void ClearConsole()
+    {
+        Assembly assembly = Assembly.GetAssembly(typeof(ActiveEditorTracker));
+        Type type = assembly.GetType("UnityEditorInternal.LogEntries");
+        if (type == null)
+        {
+            type = assembly.GetType("UnityEditor.LogEntries");
+        }
+
+        MethodInfo method = type.GetMethod("Clear");
+        method.Invoke(new object(), null);
+    }
+}
