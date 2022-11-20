4f185832d0e3ec8d08b64947a47f45b77ec8d8fa
zhanglei <>
2022-11-20 11:19:19 +0800

    MetaAssetImporter.cs

--- UnityEditorTools/Assets/Editor/ToPinYin/MetaAssetImporter.cs
+++ UnityEditorTools/Assets/Editor/ToPinYin/MetaAssetImporter.cs
@@ -4,6 +4,26 @@ using UnityEngine;
 
 public class MetaAssetImporter : AssetPostprocessor
 {
+    [MenuItem("Assets/ToPinYin")]
+    public static void Select2Pinyin()
+    {
+        string[] strs = Selection.assetGUIDs;
+        for (var i = 0; i < strs.Length; i++)
+        {
+            string path = AssetDatabase.GUIDToAssetPath(strs[i]);
+            string filePath = path.Substring(7);
+            string[] fileInfoTemp = path.Split('/');
+            string fileName = fileInfoTemp[fileInfoTemp.Length - 1];
+            string pinYin = PinYin.MakeSpellCode(fileName, SpellOptions.EnableUnicodeLetter);
+
+            string sourceFile = Path.Combine(Application.dataPath, filePath);
+            string toFile = sourceFile.Replace(fileName, pinYin);
+            File.Move(sourceFile, toFile);
+        }
+
+        AssetDatabase.Refresh();
+    }
+    
     public static void OnPostprocessAllAssets(string[] importedAsset, string[] deletedAssets, string[] movedAssets,
         string[] movedFromAssetPaths)
     {
